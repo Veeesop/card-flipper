71217878f71d6bc8ddaeac18ae058faffbc5bde9
Alexander Ratanas <alexanderratanas@gmail.com>
2022-08-25 17:47:58 -0500

    added shuffle deck function

--- src/model/deck.ts
+++ src/model/deck.ts
@@ -9,13 +9,25 @@ export class Deck {
     }
 
     private newDeck = (): Card[] => {
-        _.forIn(Suit, (value, _key) => {
+        _.forIn(Suit, (suit, _key) => {
             for( let i = 2; i <= 14; i++) {
-                const newCard = new Card(value, i);
+                const newCard = new Card(suit, i);
                 newCard.generateName()
                 this.cards.push(newCard)
             }
         })
         return this.cards;
+    }
+
+    public shuffleDeck = (counter: number = 3, deck:  Card[] = this.newDeck()): Card[] => {
+        if (counter === 0) return deck;
+
+        for( let i = 51; i >= 0; i --){
+            const randomIndex = Math.floor(Math.random() * i);
+            let tempDeck: Card = deck[i];
+            deck[i] = deck[randomIndex];
+            deck[randomIndex] = tempDeck;
+        }
+        return this.shuffleDeck(counter -1, deck);
     }
 } 
